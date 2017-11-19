87ce1a8683e029af3d9de113659fcb44a78357db
Wilhelm Klopp <wil.klopp@gmail.com>
2017-11-19 18:32:42 +0000

    Move to SingleScreen

--- index.android.js
+++ index.android.js
@@ -19,32 +19,9 @@ const store = createStore(
 persistStore(store, { storage: AsyncStorage });
 registerScreens(store, Provider);
 
-const createTabs = () => {
-  const tabs = [
-    {
-      screen: 'veruto.HomeScreen',
-      icon: require('./images/home.png'),
-      selectedIcon: require('./images/home_selected.png'),
-      title: 'Closest Rooms',
-    },
-    {
-      screen: 'veruto.MapScreen',
-      icon: require('./images/three.png'),
-      selectedIcon: require('./images/three_selected.png'),
-      title: 'Closest Rooms',
-      navigatorStyle: {
-        tabBarBackgroundColor: '#4dbce9',
-      },
-    },
-  ];
-  return tabs;
-};
-
-Navigation.startTabBasedApp({
-  tabs: createTabs(),
-  appStyle: {
-    tabBarBackgroundColor: '#0f2362',
-    tabBarButtonColor: '#ffffff',
-    tabBarSelectedButtonColor: '#63d7cc',
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'veruto.HomeScreen',
+    title: 'Closest Rooms',
   },
 });
